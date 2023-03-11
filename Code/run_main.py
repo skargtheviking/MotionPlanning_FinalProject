3b702f970464b360901086ae89c68feb91d8dcc5
skargtheviking <112199008+skargtheviking@users.noreply.github.com>
2023-03-11 14:33:16 -0700

    First Commit
    First Commit

---
+++ Code/run_main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plotter
+from math import pi
+import time
+
+Number_of_heros = 1                     ## How many heros will be used
+Known = True                            ## Is the map already known?
+cultists = False                        ## Are we using cultitsts?
+
+heros = ["Ultra_Instinct_Shaggy"]       ## lists all the heros used
